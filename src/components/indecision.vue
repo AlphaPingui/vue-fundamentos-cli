49c7acbd7e214648da09515f91d555f862fa92ba
Juan Antonio Martínez <jafuentes@aunnait.es>
2023-03-24 19:12:25 +0100

    Video 61 terminado

--- src/components/indecision.vue
+++ src/components/indecision.vue
@@ -1,5 +1,5 @@
 <template>
-    <img src="https://via.placeholder.com/250" alt="BG">
+    <img v-if="img" :src="img" alt="BG">
     <div class="bg-dark"></div>
 
     <div class="indecision-container">
@@ -7,7 +7,7 @@
         <p>Recuerda terminar con un signo de interrogación (?)</p>
         <div>
             <h2>{{ question }}</h2>
-            <h1>Si, No, ...pensando</h1>
+            <h1>{{ answer }}</h1>
         </div>
     </div>
 </template>
@@ -17,12 +17,22 @@ export default {
     data() {
         return {
             question: null,
+            answer: null,
+            img: null,
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = 'Pensando...';
+            const {answer, image} = await fetch('https://yesno.wtf/api').then(r => r.json());
+            this.answer = answer;
+            this.img = image;
         }
     },
     watch: {
         question(value, oldValue) {
             if (!value.includes('?')) return;
-            // TODO Realizar peticion http
+            this.getAnswer();
         }
     }
 }
