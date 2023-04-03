1361d5135649fd40c9db411c0a6ff30bf90a5d90
Juan Antonio Martínez <jafuentes@aunnait.es>
2023-04-03 09:08:29 +0200

    Video 68 terminado

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('Example Component', () => {
+  test('Debe de ser mayor que 10',  () => {
+    // AAA -> Arrange
+    let value = 10;
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    // AAA -> Act
+    value = value + 2;
+
+    // AAA -> Assert
+    expect(value).toBeGreaterThan(10);
   })
 })
